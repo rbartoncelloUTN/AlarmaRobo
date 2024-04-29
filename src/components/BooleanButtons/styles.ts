5fc888cf63a4e1019bb80c1f8544d8400bda349a
rbartoncelloUTN <r.bartoncello@sistemas-utnfra.com.ar>
2024-04-28 21:02:34 -0300

    feat(login): add login

---
+++ src/components/BooleanButtons/styles.ts
@@ -0,0 +1,49 @@
+import theme from '../../theme/base';
+
+export const createStyles = () => ({
+  container: {
+    width: '100%',
+    marginBottom: theme.spacing.marginVertical * 1.5,
+    alignItems: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    color: theme.colors.black,
+    fontSize: theme.typography.regular.fontSize,
+    paddingBottom: theme.spacing.paddingVertical / 2,
+  },
+  content: {
+    alignItems: 'center',
+    width: '100%',
+  },
+  buttonGroupContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '100%',
+    minWidth: 250,
+    height: 50,
+    borderColor: theme.colors.black,
+  },
+  selectedButton: {
+    backgroundColor: theme.colors.brandPrimary,
+  },
+  selectedText: {
+    color: theme.colors.white,
+  },
+  text: {
+    color: 'black',
+  },
+  disabled: {
+    opacity: 0.7,
+    backgroundColor: theme.colors.disabled,
+  },
+  captionContainer: {
+    flexDirection: 'row',
+    width: '50%',
+  },
+  caption: {
+    fontWeight: 'bold',
+    width: '50%',
+    textAlign: 'center',
+  },
+});
